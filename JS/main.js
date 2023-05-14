baa8141a5b1cf2a15b5b676b1c6bee5a8ae36ecd
ismail-cherrak <ismailcherrak481@gmail.com>
2023-05-14 11:33:45 +0100

    a modifier voir trello

--- JS/main.js
+++ JS/main.js
@@ -1,156 +1,150 @@
-const slider = document.querySelector('.scroll');
-const sliderWrapper = slider.querySelector('.sld');
-const slides = slider.querySelectorAll('.book');
-const prevBtn = slider.querySelector('.scroll_left');
-const nextBtn = slider.querySelector('.scroll_right');
-const slideWidth = slides[0].offsetWidth;
-const totalSlides = slides.length;
-let currentIndex = 0;
-prevBtn.style.display="none";
-
-sliderWrapper.style.transition = 'transform 0.3s ease';
-
-nextBtn.addEventListener('click', () => {
-    currentIndex++;
-    prevBtn.style.display="flex";
-
-  sliderWrapper.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
-});
-
-prevBtn.addEventListener('click', () => {
-
-    currentIndex--;
-if(currentIndex==0){
-    prevBtn.style.display="none";
-}
-  
-  sliderWrapper.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
-});
-
-
-
 //book state 
 
-let etat=0;//availabel=0;notavailable=1;waiting=2;ready to take=3;taken=4
+let etatone=0;//availabel=0;notavailable=1;waiting=2;ready to take=3;taken=4
 
 function a(n){
     switch(n){
         case 0:
-            document.getElementById("state").innerHTML="Available";
-            document.getElementById("state").style.color="green";
-            document.getElementById("state").style.textShadow="0px 0px 4px green";
-            document.getElementById("ifelse").style.cursor="pointer";
-            document.getElementById("ifelse").innerHTML="order";
-            document.getElementById("ifelse").style.color="aqua";
-            document.getElementById("ifelse").onclick=function(){
+            document.querySelector(".stateone").innerHTML="Available";
+            document.querySelector(".stateone").style.color="green";
+            document.querySelector(".stateone").style.textShadow="0px 0px 4px green";
+            document.querySelector(".ifelseone").style.cursor="pointer";
+            document.querySelector(".ifelseone").innerHTML="Order";
+            document.querySelector(".ifelseone").style.color="aqua";
+          /*  document.querySelector(".ifelseone").onclick=function(){
                document.querySelector("body").style.opacity=0.8;
-               document.getElementById("sur").classList.remove("hide");
-               document.getElementById("outone").onclick=function(){
-                etat=2;
+               document.getElementById("surone").classList.remove("hide");
+               document.querySelector(".outoneone").onclick=function(){
+                etatone=2;
                 a(2);
-                document.getElementById("sur").classList.add("hide");
+                document.getElementById("surone").classList.add("hide");
                 document.querySelector("body").style.opacity=1;
                }
-               document.getElementById("outtwo").onclick=function(){
-                document.getElementById("sur").classList.add("hide");
+               document.querySelector(".outtwoone").onclick=function(){
+                document.getElementById("surone").classList.add("hide");
                 document.querySelector("body").style.opacity=1;
                }
-            }
+            }*/
             break;
         case 1:
-                document.getElementById("state").innerHTML="Not Available";
-                document.getElementById("state").style.color="red";
-                document.getElementById("ifelse").style.opacity="0.5";
+                document.querySelector(".stateone").innerHTML="Not Available";
+                document.querySelector(".stateone").style.color="red";
+                document.querySelector(".ifelseone").style.opacity="0.5";
         break;
         case 2:
-            document.getElementById("state").innerHTML="Waiting";
-            document.getElementById("state").style.color="yellow";
-            document.getElementById("ifelse").innerHTML="Cancel";
-            document.getElementById("ifelse").style.color="red";
-            document.getElementById("ifelse").style.cursor="pointer";
-            document.getElementById("ifelse").onclick=function(){
+            document.querySelector(".stateone").innerHTML="Waiting";
+            document.querySelector(".stateone").style.color="yellow";
+            document.querySelector(".ifelseone").innerHTML="Cancel";
+            document.querySelector(".ifelseone").style.color="red";
+            document.querySelector(".ifelseone").style.cursor="pointer";
+           /* document.querySelector(".ifelseone").onclick=function(){
                 document.querySelector("body").style.opacity=0.8;
-                document.getElementById("sur").classList.remove("hide");
-                document.getElementById("outone").onclick=function(){
-                 etat=0;
+                document.getElementById("surone").classList.remove("hide");
+                document.querySelector(".outoneone").onclick=function(){
+                 etatone=0;
                  a(0);
-                 document.getElementById("sur").classList.add("hide");
+                 document.getElementById("surone").classList.add("hide");
                  document.querySelector("body").style.opacity=1;
                 }
-                document.getElementById("outtwo").onclick=function(){
-                 document.getElementById("sur").classList.add("hide");
+                document.querySelector(".outtwoone").onclick=function(){
+                 document.getElementById("surone").classList.add("hide");
                  document.querySelector("body").style.opacity=1;
                 }
-             }
+             }*/
              break;
         case 3:
-            document.getElementById("state").innerHTML="Ready to take";
-            document.getElementById("state").style.color="blue";
-            document.getElementById("ifelse").innerHTML="Cancel";
-            document.getElementById("ifelse").style.color="red";
-            document.getElementById("ifelse").style.cursor="pointer";
-            document.getElementById("ifelse").onclick=function(){
+            document.querySelector(".stateone").innerHTML="Ready to take";
+            document.querySelector(".stateone").style.color="blue";
+            document.querySelector(".ifelseone").innerHTML="Cancel";
+            document.querySelector(".ifelseone").style.color="red";
+            document.querySelector(".ifelseone").style.cursor="pointer";
+           /* document.querySelector(".ifelseone").onclick=function(){
                 document.querySelector("body").style.opacity=0.8;
-                document.getElementById("sur").classList.remove("hide");
-                document.getElementById("outone").onclick=function(){
-                 etat=0;
+                document.getElementById("surone").classList.remove("hide");
+                document.querySelector(".outoneone").onclick=function(){
+                 etatone=0;
                  a(0);
-                 document.getElementById("sur").classList.add("hide");
+                 document.getElementById("surone").classList.add("hide");
                  document.querySelector("body").style.opacity=1;
                 }
-                document.getElementById("outtwo").onclick=function(){
-                 document.getElementById("sur").classList.add("hide");
+                document.querySelector(".outtwoone").onclick=function(){
+                 document.getElementById("surone").classList.add("hide");
                  document.querySelector("body").style.opacity=1;
                 }
-             }
+             }*/
              break;
         case 4:
             //time left
-             document.getElementById("yesno").style.opacity="0";
-
-            
-}
+             document.querySelector(".yesnoone").style.opacity="0";
+    }
 }
 
 
-a(etat); 
-
+a(etatone);
 
 //rating 
-let booksRating =0.0;
+let booksRatingone =4;
 
 
 
-function putRating(r){
+function putRatingone(r,roundone){
     
-    switch(Math.round(r,round,rating)){
+    switch(Math.round(r)){
         case 0 :
-            round.style.marginLeft="00px";
-            rating.innerHTML=Math.round(r); 
+            roundone.style.marginLeft="00px"; 
         break;
         case 1 :
-            round.style.marginLeft="12px";
-            rating.innerHTML=Math.round(r);
+            roundone.style.marginLeft="12px";
         break;
         case 2 :
-            round.style.marginLeft="24px";
-            rating.innerHTML=Math.round(r);
+            roundone.style.marginLeft="24px";
         break;
         case 3 :
-            round.style.marginLeft="36px";
-            rating.innerHTML=Math.round(r);
+            roundone.style.marginLeft="400px";
         break;
         case 4 :
-            round.style.marginLeft="48px";
-            rating.innerHTML=Math.round(r);
+            roundone.style.marginLeft="48px";
         break;
         case 5 :
-            round.style.marginLeft="60px";
-            rating.innerHTML=Math.round(r);
+            roundone.style.marginLeft="60px";
         break;
         
     }
     
 }
-//putRating(4,document.getElementById("round"),document.getElementById("the_average_rating"));
-alert("hi");
+putRatingone(document.getElementsByClassName("the_average_ratingone")[0],document.getElementsByClassName("roundone")[0]);
+
+
+
+
+
+//SWIPER:
+
+const sliderone = document.querySelector('.scrollone');
+const sliderWrapperone = sliderone.querySelector('.sldone');
+const slidesone = sliderone.querySelectorAll('.bookone');
+const prevBtnone = sliderone.querySelector('.scroll_leftone');
+const nextBtnone = sliderone.querySelector('.scroll_rightone');
+const slideWidthone = slidesone[0].offsetWidth;
+const totalSlidesone = slidesone.length;
+let currentIndexone = 0;
+prevBtnone.style.display="none";
+
+sliderWrapperone.style.transition = 'transform 0.3s ease';
+
+nextBtnone.addEventListener('click', () => {
+    currentIndexone++;
+    prevBtnone.style.display="flex";
+
+  sliderWrapperone.style.transform = `translateX(${-slideWidthone * currentIndexone}px)`;
+});
+
+prevBtnone.addEventListener('click', () => {
+
+    currentIndexone--;
+if(currentIndexone==0){
+    prevBtnone.style.display="none";
+}
+  
+  sliderWrapperone.style.transform = `translateX(${-slideWidthone * currentIndexone}px)`;
+});
